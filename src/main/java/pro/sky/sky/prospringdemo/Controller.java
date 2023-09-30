e902251602a998e26d8a006bc85992adb4652d32
Den <tequila96@bk.ru>
2023-09-30 13:14:09 +0500

    Исправил

--- src/main/java/pro/sky/sky/prospringdemo/Controller.java
+++ src/main/java/pro/sky/sky/prospringdemo/Controller.java
@@ -1,6 +1,7 @@
 package pro.sky.sky.prospringdemo;
 
 
+import com.sun.source.tree.BreakTree;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class Controller {
+    private final Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
     @GetMapping(path = "/calculator")
     public String hello() {
         return "Добро пожаловать в калькулятор";
@@ -15,24 +22,25 @@ public class Controller {
 
     @GetMapping(path = "/calculator/plus")
     public String sum(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 + num2;
+        int result = service.sum(num1, num2);
         return num1 + " + " + num2 + " = " + result;
     }
 
     @GetMapping(path = "/calculator/minus")
     public String minus(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 - num2;
+        int result = service.minus(num1, num2);
         return num1 + " - " + num2 + " = " + result;
     }
 
     @GetMapping(path = "/calculator/multiply")
     public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 * num2;
+        int result = service.multiply(num1,num2);
         return num1 + " * " + num2 + " = " + result;
     }
+
     @GetMapping(path = "/calculator/divide")
     public String divide(@RequestParam int num1, @RequestParam int num2) {
-        int result = num1 / num2;
+        int result = service.divide(num1, num2);
         return num1 + " / " + num2 + " = " + result;
     }
-}
+    }
---
+++ src/main/java/pro/sky/sky/prospringdemo/Service.java
@@ -0,0 +1,19 @@
+package pro.sky.sky.prospringdemo;
+@org.springframework.stereotype.Service
+public class Service {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
